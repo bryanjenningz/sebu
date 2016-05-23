f1422dbfe77392df77a6db9d0413b76470655b0d
Bryan Jennings <bryanjenningz@gmail.com>
2016-05-23 11:54:47 -0700

    add srs constants, slash button, toggleList action creator

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-var KEYS = {S: 83, X: 88}
+var KEYS = {S: 83, X: 88, SLASH: 191}
+var slashDown = false
+
+var MINUTE = 60000
+var HOUR = 3600000
+var intervals = [
+  MINUTE,
+  2 * MINUTE,
+  4 * MINUTE,
+  8 * MINUTE,
+  15 * MINUTE,
+  30 * MINUTE,
+  HOUR,
+  2 * HOUR,
+  4 * HOUR,
+  8 * HOUR,
+  12 * HOUR,
+  24 * HOUR
+]
+
 var el = React.createElement.bind(React)
 var root = document.createElement('div')
 root.setAttribute('id', 'root')
@@ -14,7 +33,7 @@ chrome.storage.sync.get('sentences', function(data) {
 var render = () => {
   ReactDOM.render(
     el(VocabList, {
-      handleClick: () => store.dispatch({type: 'TOGGLE_LIST'}),
+      handleClick: toggleList,
       items: store.getState().items,
       visible: store.getState().visible
     }),
@@ -26,10 +45,12 @@ var addItem = item => {
   store.dispatch({type: 'ADD_ITEM', item: item})
   chrome.storage.sync.set({sentences: store.getState().items})
 }
-
 var removeItem = index => {
   store.dispatch({type: 'REMOVE_ITEM', index})
   chrome.storage.sync.set({sentences: store.getState().items})
+}
+var toggleList = () => {
+  store.dispatch({type: 'TOGGLE_LIST'})
 }
 
 var reducer = (state = {
@@ -81,7 +102,7 @@ var VocabList = ({
   visible
 }) => (
   el('div', {style: Object.assign({}, popupStyle, {display: visible ? 'block' : 'none'})},
-    el('span', {onClick: () => store.dispatch({type: 'TOGGLE_LIST'})}, 'X'),
+    el('span', {onClick: handleClick}, 'X'),
     items.map((item, i) => (
       el('li', {key: i},
         el('span', {}, item),
@@ -97,17 +118,28 @@ function popup(text) {
 
 addEventListener('keydown', function(e) {
   var selection = getSelection().toString()
-  if (e.keyCode === KEYS.S && selection.length > 0) {
-    chrome.storage.sync.get('sentences', function(data) {
-      var sentences = Array.isArray(data.sentences) && data.sentences.length > 0 ? data.sentences : []
-      chrome.storage.sync.set({sentences: JSON.stringify(sentences.concat(selection))}, function() {
-        popup('Saved: ' + selection)
-        addItem(selection)
+  if (slashDown) {
+    if (e.keyCode === KEYS.S && selection.length > 0) {
+      chrome.storage.sync.get('sentences', function(data) {
+        var sentences = Array.isArray(data.sentences) && data.sentences.length > 0 ? data.sentences : []
+        chrome.storage.sync.set({sentences: JSON.stringify(sentences.concat(selection))}, function() {
+          popup('Saved: ' + selection)
+          addItem(selection)
+        })
       })
-    })
-  } else if (e.keyCode === KEYS.X) {
-    store.dispatch({type: 'TOGGLE_LIST'})
+    } else if (e.keyCode === KEYS.X) {
+      toggleList()
+    }
+  } else if (e.keyCode === KEYS.SLASH) {
+    slashDown = true
+  }
+})
+
+addEventListener('keyup', function(e) {
+  if (slashDown && e.keyCode === KEYS.SLASH) {
+    slashDown = false
   }
 })
 
 console.log('sebu start!')
+
