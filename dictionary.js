d6e1838ebad39a4ed06b390286db49c4ec723776
Bryan Jennings <bryanjenningz@gmail.com>
2016-05-25 01:10:17 -0700

    add dictionary

--- dictionary.js
+++ dictionary.js
@@ -1,6 +1,6 @@
 var dictionary = (() => {
   var request = new XMLHttpRequest()
-  request.open('GET', 'data/dictionary.dat', false)
+  request.open('GET', 'data/dictionary.txt', false)
   request.send(null)
   return request.responseText
 })()
